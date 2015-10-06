17eb2e052de5a3cdb374769df3922cda680f27c1
maclover7 <me@jonathanmoss.me>
2015-10-05 20:27:29 -0400

    remove failing test

--- spec/megaphone_spec.rb
+++ spec/megaphone_spec.rb
@@ -4,8 +4,4 @@ describe Megaphone do
   it 'has a version number' do
     expect(Megaphone::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
